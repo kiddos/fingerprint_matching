bdd3d20dc7b1574031c26af15ca260b37b4e93dd
kiddos <josephyu831007@yahoo.com.tw>
2016-05-28 21:46:07 +0800

    dont use pointer

--- src/minutiaset.h
+++ src/minutiaset.h
@@ -15,12 +15,14 @@ typedef struct minutiaset_t {
   Minutiae *set;
 } MinutiaSet;
 
-MinutiaRawSet *minutiarawset_load(const char* const filepath);
-MinutiaSet *minutiaset_create(MinutiaRawSet *rawset, int refindex);
-int minutiaset_compute_num_matched(MinutiaSet *q, MinutiaSet *r);
-void minutiarawset_info(MinutiaRawSet *rawset);
-void minutiaset_info(MinutiaSet *set);
-void minutiarawset_release(MinutiaRawSet *set);
-void minutiaset_release(MinutiaSet *set);
+MinutiaRawSet minutiarawset_create_empty();
+MinutiaRawSet minutiarawset_load(const char* const filepath, bool *success);
+MinutiaSet minutiaset_create_empty();
+MinutiaSet minutiaset_create(MinutiaRawSet rawset, int refindex);
+int minutiaset_compute_num_matched(MinutiaSet query, MinutiaSet reference);
+void minutiarawset_info(MinutiaRawSet rawset);
+void minutiaset_info(MinutiaSet set);
+void minutiarawset_release(MinutiaRawSet set);
+void minutiaset_release(MinutiaSet set);
 
 #endif /* end of include guard: MINUTIASET_H */
