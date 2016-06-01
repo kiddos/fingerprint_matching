6aef144064fa0cb0db28fe5d789ad54e5ba22d6f
kiddos <josephyu831007@yahoo.com.tw>
2016-06-01 10:38:49 +0800

    separate basic math

---
+++ src/basicmath.h
@@ -0,0 +1,9 @@
+#ifndef BASICMATH_H
+#define BASICMATH_H
+
+extern const double PI;
+extern const double SQRT2;
+
+double arctan(double dx, double dy);
+
+#endif /* end of include guard: BASICMATH_H */
--- src/common.h
+++ src/common.h
@@ -5,15 +5,11 @@
 #include <string.h>
 #include <math.h>
 
+#include "basicmath.h"
 #include "type.h"
 #include "vec.h"
 #include "point.h"
 #include "line.h"
 #include "polygon.h"
-
-extern const double PI;
-extern const double SQRT2;
-
-double arctan(double dx, double dy);
 
 #endif /* end of include guard: COMMON_H */
