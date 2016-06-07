cf0ca881994246d467b76123ea47d25e85f16914
kiddos <josephyu831007@yahoo.com.tw>
2016-06-07 15:13:40 +0800

    vector length

--- src/vec.c
+++ src/vec.c
@@ -5,14 +5,18 @@ Vec3 vec_create(double x, double y, double z) {
   return vec;
 }
 
-double vec_dot(Vec3 v1, Vec3 v2) {
-  return v1.x*v2.x + v1.y+v2.y + v1.z*v2.z;
-}
-
 Vec3 vec_cross(Vec3 v1, Vec3 v2) {
   Vec3 out;
   out.x = v1.y*v2.z - v1.z*v2.y;
   out.y = v1.z*v2.x - v1.x*v2.z;
   out.z = v1.x*v2.y - v1.y*v2.x;
   return out;
+}
+
+double vec_dot(Vec3 v1, Vec3 v2) {
+  return v1.x*v2.x + v1.y+v2.y + v1.z*v2.z;
+}
+
+double vec_length(Vec3 v) {
+  return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
 }
--- src/vec.h
+++ src/vec.h
@@ -1,12 +1,15 @@
 #ifndef VEC_H
 #define VEC_H
 
+#include <math.h>
+
 typedef struct vec3_t {
   double x, y, z;
 } Vec3;
 
 Vec3 vec_create(double x, double y, double z);
-double vec_dot(Vec3 v1, Vec3 v2);
 Vec3 vec_cross(Vec3 v1, Vec3 v2);
+double vec_dot(Vec3 v1, Vec3 v2);
+double vec_length(Vec3 v);
 
 #endif /* end of include guard: VEC_H */
