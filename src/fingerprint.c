9920559f9fbaed1aa8b45ac1475af034fca8ad53
kiddos <josephyu831007@yahoo.com.tw>
2016-06-14 20:24:21 +0800

    add secondaryfeatureset

--- src/fingerprint.c
+++ src/fingerprint.c
@@ -1,7 +1,8 @@
 #include "fingerprint.h"
 
 Fingerprint fingerprint_create_empty() {
-  Fingerprint fp = {0, 0, NULL, minutiarawset_create_empty(), NULL};
+  Fingerprint fp = {0, 0, NULL, minutiarawset_create_empty(), NULL,
+                    secondaryfeatureset_create_empty()};
   return fp;
 }
 
@@ -31,10 +32,11 @@ Fingerprint fingerprint_load(const char *const filepath, bool *success) {
     strcpy(fp.name, filename);
     fp.rawset = rawset;
     fp.numset = rawset.size;
-    fp.set = (MinutiaSet *) calloc(fp.numset, sizeof(MinutiaSet));
+    fp.mset = (MinutiaSet *) calloc(fp.numset, sizeof(MinutiaSet));
     for (i = 0 ; i < rawset.size ; ++i) {
-      fp.set[i] = minutiaset_create(rawset, i);
+      fp.mset[i] = minutiaset_create(rawset, i);
     }
+    fp.sset = secondaryfeatureset_create(fp.rawset);
     return fp;
   }
 
@@ -42,18 +44,18 @@ Fingerprint fingerprint_load(const char *const filepath, bool *success) {
   return fp;
 }
 
-int fingerprint_get_num_matched_minutia(Fingerprint query,
-                                        Fingerprint reference) {
+int fingerprint_get_num_matched_minutia(const Fingerprint query,
+                                        const Fingerprint reference) {
   int count = 0, i, j;
   for (i = 0 ; i < reference.numset ; ++i) {
     for (j = 0 ; j < query.numset ; ++j) {
-      count += minutiaset_compute_num_matched(query.set[j], reference.set[i]);
+      count += minutiaset_compute_num_matched(query.mset[j], reference.mset[i]);
     }
   }
   return count;
 }
 
-int fingerprint_get_num_minutia(Fingerprint fp) {
+int fingerprint_get_num_minutia(const Fingerprint fp) {
   return fp.rawset.size;
 }
 
@@ -66,14 +68,15 @@ SecondaryFeature fingerprint_get_center_secondary_feature(Fingerprint fp) {
   return sf;
 }
 
-bool fingerprint_match_secondary_feature(Fingerprint fp1, Fingerprint fp2) {
+bool fingerprint_match_secondary_feature(const Fingerprint fp1,
+                                         const Fingerprint fp2) {
   const SecondaryFeature sf1 = fingerprint_get_center_secondary_feature(fp1);
   const SecondaryFeature sf2 = fingerprint_get_center_secondary_feature(fp2);
 
   return secondaryfeature_is_matched(sf1, sf2);
 }
 
-void fingerprint_info(Fingerprint fp) {
+void fingerprint_info(const Fingerprint fp) {
 #ifdef DEBUG
   if (fp.name != NULL)
     printf("name of fingerprint: %s\n", fp.name);
@@ -86,7 +89,7 @@ void fingerprint_info(Fingerprint fp) {
 #ifdef DEBUG
     printf("minutia set %d:\n", i);
 #endif
-    minutiaset_info(fp.set[i]);
+    minutiaset_info(fp.mset[i]);
   }
 }
 
@@ -96,6 +99,7 @@ void fingerprint_release(Fingerprint fp) {
 
   minutiarawset_release(fp.rawset);
   for (i = 0 ; i < fp.numset ; ++i) {
-    minutiaset_release(fp.set[i]);
+    minutiaset_release(fp.mset[i]);
   }
+  secondaryfeatureset_release(fp.sset);
 }
--- src/fingerprint.h
+++ src/fingerprint.h
@@ -3,23 +3,26 @@
 
 #include "minutiaset.h"
 #include "type.h"
-#include "secondaryfeature.h"
+#include "secondaryfeatureset.h"
 
 typedef struct fingerprint_t {
   int numset;
   int id;
   char *name;
   MinutiaRawSet rawset;
-  MinutiaSet *set;
+  MinutiaSet *mset;
+  SecondaryFeatureSet sset;
 } Fingerprint;
 
 Fingerprint fingerprint_create_empty();
 Fingerprint fingerprint_load(const char *const filepath, bool *success);
-int fingerprint_get_num_matched_minutia(Fingerprint query, Fingerprint reference);
-int fingerprint_get_num_minutia(Fingerprint fp);
-SecondaryFeature fingerprint_get_center_secondary_feature(Fingerprint fp);
-bool fingerprint_match_secondary_feature(Fingerprint fp1, Fingerprint fp2);
-void fingerprint_info(Fingerprint fp);
+int fingerprint_get_num_matched_minutia(const Fingerprint query,
+                                        const Fingerprint reference);
+int fingerprint_get_num_minutia(const Fingerprint fp);
+SecondaryFeature fingerprint_get_center_secondary_feature(const Fingerprint fp);
+bool fingerprint_match_secondary_feature(const Fingerprint fp1,
+                                         const Fingerprint fp2);
+void fingerprint_info(const Fingerprint fp);
 void fingerprint_release(Fingerprint fp);
 
 #endif /* end of include guard: FINGERPRINT_H */
