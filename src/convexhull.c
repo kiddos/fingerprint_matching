278c7694adb95fe30087f29449b49627a828c420
kiddos <josephyu831007@yahoo.com.tw>
2016-06-15 05:58:51 +0800

    convexhull module

---
+++ src/convexhull.c
@@ -0,0 +1,2 @@
+#include "convexhull.h"
+
--- src/convexhull.h
+++ src/convexhull.h
@@ -4,9 +4,14 @@
 #include "polygon.h"
 #include "minutiaset.h"
 
+// solve convexhull directly relate the minutia
 typedef struct convexhull_t {
-  Polygon poly;
-  MinutiaRawSet set;
+  Polygon shape;
+  MinutiaRawSet refset;
 } ConvexHull;
+
+ConvexHull convexhull_create(const MinutiaRawSet set);
+bool convexhull_is_minutiaeraw_in(const ConvexHull convexhull,
+                                  const MinutiaeRaw p);
 
 #endif /* end of include guard: CONVEXHULL_H */
